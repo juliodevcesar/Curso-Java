97bf205c361db8f33aa0cf5bcf1159107511cca5
Julio <julio17cs@gmail.com>
2026-08-29 16:28:09 -0300

    Exercicio 04

---
+++ src/capitulo03/Exercicio04.java
@@ -0,0 +1,36 @@
+package capitulo03;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite o valor do produto: ");
+		Double valorProduto = scanner.nextDouble();
+			
+		System.out.println("Digite a quantidade que deseja: ");
+		Integer quantidadeProduto = scanner.nextInt();
+		
+		Double subtotalProdutos = valorProduto * quantidadeProduto;
+		
+		Boolean quantidadeMaiorOuIgualDez = quantidadeProduto >= 10;
+		
+		Double percentualDesconto = 0.0;
+		
+		if (quantidadeMaiorOuIgualDez) {
+			percentualDesconto = 10.0;
+		}
+		
+		Double desconto = subtotalProdutos * percentualDesconto / 100;
+		
+		Double valorTotalDesconto = subtotalProdutos - desconto;
+		
+		System.out.println("Valor total: " + valorTotalDesconto);
+		
+		scanner.close();
+		
+		
+		
+	}
+}
