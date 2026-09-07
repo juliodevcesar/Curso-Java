8f8f693edeb0f05d0eee54081512e833fa5aa633
Julio <julio17cs@gmail.com>
2026-09-07 16:05:37 -0300

    Atualizando

--- src/capitulo05/UtilizandoIfEncadeado.java
+++ src/capitulo05/UtilizandoIfEncadeado.java
@@ -10,20 +10,40 @@ public class UtilizandoIfEncadeado {
 		System.out.println("Digite o peso: ");
 		Double peso = scanner.nextDouble();
 		
-		Boolean pesoLeve = peso <= 60;
-		Boolean pesoMedio = (peso > 60) && (peso <= 90);
+//		Boolean pesoLeve = peso <= 60;
+//		Boolean pesoMedio = (peso > 60) && (peso <= 90);
+//		Boolean pesoPesado = peso > 90;
+//		
+//		if (pesoLeve) {
+//			System.out.println("Categoria peso leve!");
+//		} else {
+//			Boolean pesoMedio = (peso > 60) && (peso <= 90);
+//			
+//			if (pesoMedio) {
+//			System.out.println("Categoria peso Médio!");
+//		} else {
+//			Boolean pesoPesado = peso > 90;
+//			
+//			if(pesoPesado) {
+//				System.out.println("Categoria peso pesado!");
+//			}
+//			
+//		}
+//			
+//	}
+	
+		Boolean pesoLeve = (peso <= 60) && (peso > 0);
+		Boolean pesoMedio = (peso > 60) && (peso <= 90);0
 		Boolean pesoPesado = peso > 90;
 		
 		if (pesoLeve) {
-			System.out.println("Categoria peso leve!");
-		}
-		
-		if (pesoMedio) {
+			System.out.println("Categoria peso Leve!");
+		} else if (pesoMedio) {
 			System.out.println("Categoria peso Médio!");
-		}
-		
-		if(pesoPesado) {
+		} else if (pesoPesado) {
 			System.out.println("Categoria peso pesado!");
+		} else {
+			System.out.println("O lutador não se encaixa em nenhuma categoria.");
 		}
 		
 		scanner.close();
