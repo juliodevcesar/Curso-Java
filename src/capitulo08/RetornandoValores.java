ee0209de156bd5efa625a4c5cdca91a39d403049
Julio <julio17cs@gmail.com>
2026-09-24 21:20:40 -0300

    RetornandoValores att

--- src/capitulo08/RetornandoValores.java
+++ src/capitulo08/RetornandoValores.java
@@ -1,27 +1,86 @@
 package capitulo08;
 
+import java.util.Scanner;
+
 public class RetornandoValores {
 
 	public static void main(String[] args) {
-//		Integer numeroQualquer = numeroAleatorio();
-//		
-//		System.out.println("Número: " + numeroQualquer);
-//	}	
-//
-//	static Integer numeroAleatorio() {
-//		Integer numero = 1;
-//		return 1;
-		Double[] premissaInicial = new Double[] {100.0, 100.0 };
-		Double proporcao = 20.0;
+		Scanner scanner = new Scanner(System.in);
 		
-		Double resultadoFinal = regraDeTres(premissaInicial, proporcao);
+		imprimirTraco();
 		
-		System.out.println("Resultado: " + resultadoFinal);
+		String[] cursos = new String[] {"Java EE", "Spring", "Java OO Avançado"};
 		
+		imprimir("Escolha dentre os cursos abaixo: ");
+		
+		iterarEExibirPosicoesDoVetorString(cursos);
+
+		Integer posicaoCursoEscolhido = receberNumeroInteiroDoUsuario("O curso que você deseja é o: ", scanner);
+		
+		Boolean posicaoValida = verificarPosicaoEscolhidaPeloUsuario(posicaoCursoEscolhido, cursos); 
+		
+		if (!posicaoValida) {
+			encerrarProgramaPorCausaDaPosicaoInvalida();
+		}
+		
+		imprimirTraco();
+		
+		String[] formasPagamento = new String[] {"Cartão", "Boleto"};
+		
+		imprimir("Escolha dentre as formas de pagamento abaixo: ");
+		
+		iterarEExibirPosicoesDoVetorString(formasPagamento);
+
+		Integer posicaoFormaPagamentoEscolhida = receberNumeroInteiroDoUsuario("Sua forma de pagamento escolhida é: ", scanner);
+		
+		posicaoValida = verificarPosicaoEscolhidaPeloUsuario(posicaoFormaPagamentoEscolhida, formasPagamento); 
+		
+		if (!posicaoValida) {
+			encerrarProgramaPorCausaDaPosicaoInvalida();
+		}
+		
+		String cursoEscolhido = cursos[posicaoCursoEscolhido];
+		String formaPagamentoEscolhida = formasPagamento[posicaoFormaPagamentoEscolhida];
+		
+		imprimirTraco();
+		
+		imprimir("O curso escolhido foi " + cursoEscolhido + " e a forma de pagamento é " + formaPagamentoEscolhida);
+		
+		scanner.close();
 	}
 	
-	static Double regraDeTres(Double[] premissa, Double proporcao) {
-		Double resultado = (premissa[0] * proporcao) / premissa[1];
-		return resultado;
- 	}
+	static Integer receberNumeroInteiroDoUsuario(String texto, Scanner scanner) {
+		imprimirEContinuarNaMesmaLinha(texto);
+		Integer numero = scanner.nextInt();
+		
+		return numero;
+	}
+	
+	static Boolean verificarPosicaoEscolhidaPeloUsuario(Integer posicao, String[] vetor) {
+		Boolean valida = posicao >= 0 && posicao < vetor.length;
+		return valida;
+	}
+	
+	static void imprimir(String texto) {
+		System.out.println(texto);
+	}
+	
+	static void imprimirEContinuarNaMesmaLinha(String texto) {
+		System.out.print(texto);
+	}
+	
+	static void iterarEExibirPosicoesDoVetorString(String[] vetor) {
+		for(int i = 0; i < vetor.length; i++) {
+			imprimir("[" + i + "] " + vetor[i]);
+		}
+	}
+	
+	static void imprimirTraco() {
+		imprimir("----------------------------------------------");
+	}
+	
+	static void encerrarProgramaPorCausaDaPosicaoInvalida() {
+		System.err.println("Posição inválida!");
+		System.exit(1);
+	}	
 }
